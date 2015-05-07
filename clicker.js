8da3fd67830d0df0f8b58d90c3d9f4112e500412
Lorenzo Candiago <lorenzocandiago@hotmail.com>
2015-05-07 19:29:38 +0000

    Default setConfigs

--- clicker.js
+++ clicker.js
@@ -1,100 +1,104 @@
 // Clicker Framework 0.1.0
 
-// Turn an element Clickable by ID
-function turnClickable(id){
-	var element = document.getElementById(id);
+var mq = window.matchMedia( "(min-width: 767px)" );
 
-	if(!element.clickable){
-		element.clickable = true;
+if (mq.matches) {
+	// Turn an element Clickable by ID
+	function turnClickable(id){
+		var element = document.getElementById(id);
 
-		element.addEventListener("click", function() {addScore(element)}, false)
+		if(!element.clickable){
+			element.clickable = true;
 
-		if(window.localStorage[element.id+"_score"] == null){
-			window.localStorage[element.id+"_score"] = "0";
+			element.addEventListener("click", function() {addScore(element)}, false)
+
+			if(window.localStorage[element.id+"_score"] == null){
+				window.localStorage[element.id+"_score"] = "0";
+			}
+			createScoreTable(element);
 		}
-		createScoreTable(element);
+
 	}
 
-}
+	// Add element score on Local Storage
+	function addScore(element){
+		window.localStorage[element.id+"_score"]++;
 
-// Add element score on Local Storage
-function addScore(element){
-	window.localStorage[element.id+"_score"]++;
+		var ele = document.getElementById(element.id + "_scoretable");
+		ele.getElementsByTagName("p")[0].innerHTML = window.localStorage[element.id+"_score"];
 
-	var ele = document.getElementById(element.id + "_scoretable");
-	ele.getElementsByTagName("p")[0].innerHTML = window.localStorage[element.id+"_score"];
-
-	console.log(window.localStorage[element.id+"_score"]);
-}
+		console.log(window.localStorage[element.id+"_score"]);
+	}
 
 
-function createScoreTable(element){
-	var dive = document.createElement("div");
-	var para = document.createElement("p");
-	var node = document.createTextNode(window.localStorage[element.id+"_score"]);
-	
-	dive.className = "score_table";
-	dive.id = element.id + "_scoretable";
-
-	var posi = getPosicaoElemento(element.id);
-
-	dive.style.left = posi.left + "px";
-	dive.style.top = posi.top + "px";
-
-	para.appendChild(node);
-	dive.appendChild(para);
-	document.body.appendChild(dive);
-}
-
-function createOnOffButton(){
-	if(!document.getElementById("on_off_button")){
+	function createScoreTable(element){
 		var dive = document.createElement("div");
 		var para = document.createElement("p");
-		var node = document.createTextNode("Let's Click!");
-
+		var node = document.createTextNode(window.localStorage[element.id+"_score"]);
+		
 		dive.className = "score_table";
-		dive.id = "on_off_button";
+		dive.id = element.id + "_scoretable";
 
-		dive.style.right = "20px";
-		dive.style.top = "15px";
+		var posi = getPosicaoElemento(element.id);
+
+		dive.style.left = posi.left + "px";
+		dive.style.top = posi.top + "px";
 
 		para.appendChild(node);
 		dive.appendChild(para);
 		document.body.appendChild(dive);
-
-		dive.addEventListener("click", function() {setConfigs()}, false);
 	}
-}
 
-function getPosicaoElemento(elemID){
-    var offsetTrail = document.getElementById(elemID);
-    var offsetLeft = 0;
-    var offsetTop = 0;
-    while (offsetTrail) {
-        offsetLeft += offsetTrail.offsetLeft;
-        offsetTop += offsetTrail.offsetTop;
-        offsetTrail = offsetTrail.offsetParent;
-    }
-    if (navigator.userAgent.indexOf("Mac") != -1 && 
-        typeof document.body.leftMargin != "undefined") {
-        offsetLeft += document.body.leftMargin;
-        offsetTop += document.body.topMargin;
-    }
-    return {left:offsetLeft, top:offsetTop};
-}
+	function createOnOffButton(){
+			var dive = document.createElement("div");
+			var para = document.createElement("p");
+			var node = document.createTextNode("Let's Click!");
 
-///
-window.addEventListener("load", function() {
-	createOnOffButton();
-}, false);
+			dive.className = "score_table";
+			dive.id = "on_off_button";
 
-function setConfigs(){
-	turnClickable("guitar1");
-	turnClickable("guitar2");
-	turnClickable("bass");
-	turnClickable("drums");
+			dive.style.right = "20px";
+			dive.style.top = "15px";
 
-	var botao = document.getElementById("on_off_button");
-	botao.getElementsByTagName("p")[0].innerHTML = "Stop the Clicks!!";
-	botao.addEventListener("click", function() {location.reload()}, false);
+			para.appendChild(node);
+			dive.appendChild(para);
+			document.body.appendChild(dive);
+
+			dive.addEventListener("click", function() {setConfigs()}, false);
+	}
+
+	function getPosicaoElemento(elemID){
+	    var offsetTrail = document.getElementById(elemID);
+	    var offsetLeft = 0;
+	    var offsetTop = 0;
+	    while (offsetTrail) {
+	        offsetLeft += offsetTrail.offsetLeft;
+	        offsetTop += offsetTrail.offsetTop;
+	        offsetTrail = offsetTrail.offsetParent;
+	    }
+	    if (navigator.userAgent.indexOf("Mac") != -1 && 
+	        typeof document.body.leftMargin != "undefined") {
+	        offsetLeft += document.body.leftMargin;
+	        offsetTop += document.body.topMargin;
+	    }
+	    return {left:offsetLeft, top:offsetTop};
+	}
+
+	///
+	window.addEventListener("load", function() {
+		if(!document.getElementById("on_off_button")){
+			createOnOffButton();
+		}
+	}, false);
+
+	function setConfigs(){
+		turnClickable("yourID1");
+		turnClickable("YourID2");
+		turnClickable("YourID3");
+		turnClickable("YourID...");
+
+		var botao = document.getElementById("on_off_button");
+		botao.getElementsByTagName("p")[0].innerHTML = "Stop the Clicks!!";
+		botao.addEventListener("click", function() {location.reload()}, false);
+	}
 }
